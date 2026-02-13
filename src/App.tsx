9062db59bd1b340347dbf5fc8cc7ae5b4072dac4
zaid <mohdzaidshaikh4026@gmail.com>
2026-02-13 08:42:23 +0530

    adding changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
 
 function App() {
+
+  const handleClick = () => {
+    alert('Button clicked!')
+  }
   return (
-    <h1>
-      Hello Webodoctor
-    </h1>
+    <>
+      <h1>
+        Hello Webodoctor
+      </h1>
+      <button onClick={handleClick}>Click me</button>
+    </>
   )
 }
 
